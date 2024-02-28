b13bab38465b69d4ca6397bd3f655179cda6bef3
Alex <alexsnider8@gmail.com>
2024-02-28 11:06:55 -0300

    refactor: create user refactored to register user with improvements"

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,55 +1,79 @@
+import {
+  CustomValidationException,
+  NotFoundException,
+} from "../controllers/customExceptions/customExceptions";
+
+import { Request, Response } from "express";
 import { prisma } from "../database";
 import argon2 from "argon2";
 
 interface User {
   username: string;
-  email: string;
   password: string;
+  email: string;
 }
 
-export const createUser = async (userData: User) => {
+export const registerUser = async (userData: User, Request: Request, Response: Response) => {
   try {
-    const { username, email, password } = userData;
+    const { username, password, email } = userData;
 
-    const userExist = await prisma.user.findUnique({
-      where: { email },
-    });
+    const endpoint = Request.originalUrl;
 
-    if (userExist) {
-      return {
-        error: true,
-        message: "User already exists",
-        statusCode: 400,
-      };
+    if (!username || !password || !email) {
+      return Response.status(400).json({ error: true, message: "Missing required fields!" });
     }
 
-    const hashedPassword = await argon2.hash(password, {
-      type: argon2.argon2id,
-      memoryCost: 1024,
-      timeCost: 3,
-      parallelism: 1,
-    });
-
-    const createdUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            username: username,
+          },
+          {
+            email: email,
+          },
+        ],
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
       },
     });
 
-    return {
-      error: false,
-      message: "User created successfully!",
-      user: createdUser,
-      statusCode: 201,
-    };
+    if (existingUser) {
+      return Response.status(400).json({ error: true, message: "User or Email already exists!" });
+    }
+
+    try {
+      const hashedPassword = await argon2.hash(password, {
+        type: argon2.argon2id,
+        memoryCost: 1024,
+        timeCost: 5,
+        parallelism: 1,
+      });
+
+      const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      Response.location(`${endpoint}/${newUser.id}`);
+
+      return Response.status(201).json({ error: false, message: "User created successfully!" });
+    } catch (error) {
+      return Response.status(400).json({ error: true, message: "Error while hashing password!" });
+    }
   } catch (error) {
-    console.log(error);
-    return {
-      error: true,
-      message: "Internal server error",
-      statusCode: 500,
-    };
+    if (error instanceof CustomValidationException) {
+      return Response.status(400).json({ error: true, message: error.message });
+    } else if (error instanceof NotFoundException) {
+      return Response.status(404).json({ error: true, message: error.message });
+    } else {
+      return Response.status(500).json({ error: true, message: "Internal server error!" });
+    }
   }
 };
