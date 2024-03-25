24bf24e46212dbbc9e42c9349535676c975b3efd
Alex <alexsnider8@gmail.com>
2024-03-25 09:40:55 -0300

    feat: [controller] add find-user method

--- src/v1/controllers/UserController.ts
+++ src/v1/controllers/UserController.ts
@@ -304,3 +304,49 @@ export const loginUser = async (userData: User, Request: Request, Response: Resp
     }
   }
 };
+
+export const findUser = async (Request: Request, Response: Response) => {
+  try {
+    const endpoint = Request.originalUrl;
+
+    const page = Request.query.page ? Number(Request.query.page) : 1;
+    const pageSize = Request.query.limit ? Number(Request.query.limit) : 10;
+
+    const offset = (page - 1) * pageSize;
+
+    const users = await prisma.users.findMany({
+      skip: offset,
+      take: pageSize,
+    });
+
+    if (!users) {
+      const logData = new LoggerPattern({
+        what: "An attempt to find users have been requested",
+        where: endpoint,
+      });
+
+      logger.log({
+        level: "warn",
+        message: logData.log(),
+        ...logData.toWinstonLog(),
+      });
+
+      return Response.status(404).json({ error: true, message: "No users found!" });
+    }
+
+    return Response.status(200).json(users);
+  } catch (error) {
+    const logData = new LoggerPattern({
+      what: "An attempt to find users origined a fatal error!",
+      where: Request.originalUrl,
+    });
+
+    logger.log({
+      level: "error",
+      message: logData.log(),
+      ...logData.toWinstonLog(),
+    });
+    console.log(error);
+    return Response.status(500).json({ error: true, message: "Oops! Something went wrong!" });
+  }
+};
