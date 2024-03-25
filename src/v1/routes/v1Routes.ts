103e901b375d8d055e0c575de001d284785c88f6
Alex <alexsnider8@gmail.com>
2024-03-25 09:38:21 -0300

    feat: [routes] add rate-limit, swagger annotations and middlewares

--- src/v1/routes/v1Routes.ts
+++ src/v1/routes/v1Routes.ts
@@ -1,34 +1,49 @@
 import { Router, Request, Response } from "express";
-import { registerUser, loginUser } from "../controllers/UserController";
-import { findUser } from "../controllers/AdminController";
-import { findCategories } from "../controllers/ManagerController";
 import { verifyToken } from "../../auth/jwt";
-import keycloak from "../keycloak_config/keycloak.config";
-import session from "express-session";
+import { corsOptions } from "../../middleware/cors";
+import { keycloakSession, keycloak } from "../../middleware/keycloak";
+import { createUserSchema, loginUserSchema } from "../../schema/user.schema";
+import { validateResource } from "../../middleware/validateResource";
+import { routeRateLimit } from "../../middleware/routeRateLimit";
+import { serverRateLimit } from "../../middleware/serverRateLimit";
+import { registerUser, loginUser } from "../controllers/UserController";
+import { findUser } from "../controllers/UserController";
+import { findCategories } from "../controllers/CategoryController";
 import dotenv from "dotenv";
 dotenv.config();
 
 const router = Router();
+router.use(serverRateLimit);
 
-// SESSION
-router.use(
-  session({
-    secret: process.env.KEYCLOAK_SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+// CORS
+router.use(corsOptions);
+
 // KEYCLOAK MIDDLEWARE
+router.use(keycloakSession);
 router.use(keycloak.middleware());
 
+// HEALTH CHECK
+/**
+ * @openapi
+ * /v1/health-check:
+ *  get:
+ *    tags: [Health Check]
+ *    description: Responds if the server is up and running
+ *    responses:
+ *      200:
+ *        description: App is up and running!
+ *      500:
+ *        description: Internal server error!
+ */
+router.get("/v1/health-check", routeRateLimit, (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
 // ADMIN ROUTES
-router.get(
-  "/v1/admin/find-all-users",
-  keycloak.protect(),
-  async (Request: Request, Response: Response) => {
-    await findUser(Request, Response);
-  }
-);
+router.get("/v1/admin/users", keycloak.protect(), async (Request: Request, Response: Response) => {
+  await findUser(Request, Response);
+});
+
 router.get(
   "/v1/admin/system-metrics",
   keycloak.protect(),
@@ -39,7 +54,7 @@ router.get(
 
 //MANAGER ROUTES
 router.get(
-  "/v1/manager/find-all-categories",
+  "/v1/manager/categories",
   keycloak.protect(),
   async (Request: Request, Response: Response) => {
     await findCategories(Request, Response);
@@ -47,24 +62,96 @@ router.get(
 );
 
 // USER ROUTES
-router.post("/v1/register", async (Request: Request, Response: Response) => {
-  const userData = Request.body;
-  await registerUser(userData, Request, Response);
-});
-router.post("/v1/login", async (Request: Request, Response: Response) => {
-  const userData = Request.body;
-  await loginUser(userData, Request, Response);
-});
-
-// VERIFY TOKEN TEST ROUTE
-router.get(
-  "/v1/verify",
-  keycloak.protect(),
-  verifyToken,
+// USER REGISTRATION DOCS
+/**
+ * @openapi
+ * '/v1/register':
+ *  post:
+ *     tags:
+ *     - User
+ *     summary: Register a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/CreateUserInput'
+ *     responses:
+ *      201:
+ *         description: User created successfully!
+ *      400:
+ *         content:
+ *            application/json:
+ *               schema:
+ *                  type: object
+ *                  properties:
+ *                     message:
+ *                        type: string
+ *                        description: Error message
+ *                        example: {
+ *                          "error": true,
+ *                          "message": "Missing required fields!"}
+ *      500:
+ *         description: Internal server error!
+ */
+router.post(
+  "/v1/register",
+  routeRateLimit,
+  validateResource(createUserSchema),
   async (Request: Request, Response: Response) => {
-    Response.clearCookie("refreshToken");
-    Response.status(200).json({ message: "Token verified!" });
+    const userData = Request.body;
+    await registerUser(userData, Request, Response);
   }
 );
+
+// USER LOGIN DOCS
+/**
+ * @openapi
+ * '/v1/login':
+ *  post:
+ *     tags:
+ *     - User
+ *     summary: Login a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/LoginUserInput'
+ *     responses:
+ *      200:
+ *         description: User logged in successfully!
+ *      400:
+ *         content:
+ *            application/json:
+ *               schema:
+ *                  type: object
+ *                  properties:
+ *                     message:
+ *                        type: string
+ *                        description: Error message
+ *                        example: {
+ *                          "error": true,
+ *                          "message": "Missing required fields!"}
+ *      404:
+ *         description: User not found!
+ *      500:
+ *         description: Internal server error!
+ */
+router.post(
+  "/v1/login",
+  routeRateLimit,
+  validateResource(loginUserSchema),
+  async (Request: Request, Response: Response) => {
+    const userData = Request.body;
+    await loginUser(userData, Request, Response);
+  }
+);
+
+// VERIFY TOKEN TEST ROUTE
+router.get("/v1/verify", verifyToken, async (Request: Request, Response: Response) => {
+  Response.clearCookie("refreshToken");
+  Response.status(200).json({ message: "Token verified!" });
+});
 
 export default router;
