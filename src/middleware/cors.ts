5407f0e230b59b4daa702aee2ce53a97f6c3f4f9
Alex <alexsnider8@gmail.com>
2024-03-27 10:47:23 -0300

    refactor: [cors] origin alterated to localhost

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 const corsOptions = router.use(
   cors({
-    origin: "*",
+    origin: "localhost",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
