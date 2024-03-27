628147724049015eabaa9435f39088b6db3c7584
Alex <alexsnider8@gmail.com>
2024-03-27 10:49:09 -0300

    feat: [cors] add options method

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -7,7 +7,7 @@ const corsOptions = router.use(
   cors({
     origin: "localhost",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
