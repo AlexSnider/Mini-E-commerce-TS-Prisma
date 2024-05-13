e2c70a641a03e7948d84445f66dfd4c8374f3b3e
AlexSnider <alexsnider8@gmail.com>
2024-05-13 15:17:15 -0300

    refactor: [JWT] refactors tokens creation modules setting "string" as main type

--- src/auth/jwt.ts
+++ src/auth/jwt.ts
@@ -12,13 +12,13 @@ interface User {
   username: string;
 }
 
-const createAccessToken = (user: User, expiresIn: number) => {
+const createAccessToken = (user: User, expiresIn: number): string => {
   const accessToken = sign(
     {
       id: user.id,
       username: user.username,
     },
-    process.env.JWT_ACCESS_TOKEN_SECRET,
+    process.env.JWT_ACCESS_TOKEN_SECRET!,
     {
       expiresIn: expiresIn,
     }
@@ -27,13 +27,13 @@ const createAccessToken = (user: User, expiresIn: number) => {
   return accessToken;
 };
 
-const createRefreshToken = (user: User, expiresIn: number) => {
+const createRefreshToken = (user: User, expiresIn: number): string => {
   const refreshToken = sign(
     {
       id: user.id,
       username: user.username,
     },
-    process.env.JWT_REFRESH_TOKEN_SECRET,
+    process.env.JWT_REFRESH_TOKEN_SECRET!,
     {
       expiresIn: expiresIn,
     }
@@ -56,7 +56,11 @@ const checkTokenHasTimeLeft = (token: string) => {
   return false;
 };
 
-const renewAccessToken = async (req: Request, res: Response, next: NextFunction) => {
+const renewAccessToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const refreshToken = req.cookies["refreshToken"];
 
@@ -67,7 +71,9 @@ const renewAccessToken = async (req: Request, res: Response, next: NextFunction)
     const decoded = verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET);
 
     if (!decoded) {
-      return res.status(401).json({ error: true, message: "Invalid refresh token!" });
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid refresh token!" });
     }
 
     const validAccessToken = await prisma.usersAccessTokens.findFirst({
@@ -139,7 +145,9 @@ const renewAccessToken = async (req: Request, res: Response, next: NextFunction)
     return next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ error: true, message: "Unable to renew access token!" });
+    return res
+      .status(401)
+      .json({ error: true, message: "Unable to renew access token!" });
   }
 };
 
@@ -151,10 +159,15 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   }
 
   try {
-    const validAccessToken = verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET);
+    const validAccessToken = verify(
+      accessToken,
+      process.env.JWT_ACCESS_TOKEN_SECRET
+    );
 
     if (!validAccessToken) {
-      return res.status(401).json({ error: true, message: "Invalid access token!" });
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid access token!" });
     }
 
     if (checkTokenHasTimeLeft(accessToken)) {
@@ -165,7 +178,9 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     return next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ error: true, message: "Oops! Something went wrong!" });
+    return res
+      .status(401)
+      .json({ error: true, message: "Oops! Something went wrong!" });
   }
 };
 
